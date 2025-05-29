9b753e5e80228d027fe0328a258cde36f3daf94e
nattanjunior <fnatanieljunior@gmail.com>
2025-05-29 20:52:23 -0300

    Adiciona Toaster e SessionProvider ao layout principal da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from 'sonner'
+import { SessionProvider } from 'next-auth/react'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +28,14 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-         cz-shortcut-listen="true"
+        cz-shortcut-listen="true"
       >
-        {children}
+        <SessionProvider>
+          {children}
+          <Toaster
+            duration={3000}
+          />
+        </SessionProvider>
       </body>
     </html>
   );
