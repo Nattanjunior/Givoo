4a4c4281c4a6ac7b7b040a0422945214eb741eff
nattanjunior <fnatanieljunior@gmail.com>
2025-05-29 19:57:33 -0300

    Cria componente de perfil do usuário com foto e informações

---
+++ src/app/dashboard/me/_components/card-profile.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Name } from "./name";
+
+interface CardProfileProps {
+    user: {
+        id: string;
+        name: string | null;
+        email: string | null;
+        username: string | null;
+        bio: string | null
+        image: string | null;
+    }
+}
+
+
+export default function CardProfile({ user }: CardProfileProps) {
+    return (
+        <section>
+            <div className="flex flex-col items-center justify-center"> 
+              <Image
+                src={user.image || "https://github.com/nattanjunior.png"}
+                alt="Foto de perfil"
+                width={96}
+                height={96}
+                className="rounded-xl bg-gray-50 object-cover border-4 border-white hover:shadow-xl duration-300"
+                priority
+                quality={100}
+              />
+            </div>
+
+            <div>
+                <Name 
+                    name={user.name ?? "Digite seu nome..."}
+                />
+            </div>
+          
+        </section>
+    )
+}
