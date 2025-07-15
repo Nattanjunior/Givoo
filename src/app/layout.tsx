8819a1280e6f2ef313efc8c8cef397c1166a2806
nattanjunior <fnatanieljunior@gmail.com>
2025-07-15 18:11:54 -0300

    fix(layout): ajusta layout global da aplicação\n\nCorrige detalhes no layout global para garantir melhor integração, exibição e experiência do usuário em toda a aplicação.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'sonner'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientContext } from "@/providers/queryclient";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +32,12 @@ export default function RootLayout({
         cz-shortcut-listen="true"
       >
         <SessionProvider>
-          {children}
-          <Toaster
-            duration={3000}
-          />
+          <QueryClientContext>
+            {children}
+            <Toaster
+              duration={3000}
+            />
+          </QueryClientContext>
         </SessionProvider>
       </body>
     </html>
